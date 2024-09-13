8d0605b0daeeb84fc664a7052596d43546f6b899
Nareshbhai Rabari <nareshrabari205@gmail.com>
2024-09-13 12:04:59 +0530

    ....

--- lib/services/google_services.dart
+++ lib/services/google_services.dart
@@ -1,9 +1,6 @@
 import 'dart:developer';
-
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleAuthService {
